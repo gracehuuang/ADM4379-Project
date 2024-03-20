9f72bce29b1813a923aa0f46f2548d08dc78e9ce
gracehuuang <41273352+gracehuuang@users.noreply.github.com>
2024-03-20 16:10:22 -0400

    Update register.php

--- ADM4379 PROJECT1/register.php
+++ ADM4379 PROJECT1/register.php
@@ -1,24 +1,7 @@
 <?php
-
-<?php
-// Database configuration
-$db_host = 'localhost'; // Change this to your database server hostname
-$db_user = 'root'; // Change this to your database username
-$db_password = 'root'; // Change this to your database password
-$db_db = 'MyTelferLife'; // Change this to your database name
-$db_port = '8889'; // Change this to your database port if needed
-
-// Create connection
-$conn = new mysqli($db_host, $db_user, $db_password, $db_db, $db_port);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Connected successfully";
-?>
 require_once "connection.php";
+$fname = $lname = $email = $password = "";
+
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $fname = $_POST['first_name'];
@@ -26,7 +9,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
     
-    $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ssss", $fname, $email, $password);
 
