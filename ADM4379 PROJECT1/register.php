2e6ec2b9eb4683ca022bccb0efb014fcea9666af
gracehuuang <41273352+gracehuuang@users.noreply.github.com>
2024-03-20 16:42:31 -0400

    Update register.php

--- ADM4379 PROJECT1/register.php
+++ ADM4379 PROJECT1/register.php
@@ -13,12 +13,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ssss", $fname, $lname, $email, $password);
     
-    echo "Before registration execution"; // Debug output
+   // echo "Before registration execution"; // Debug output
     if ($stmt->execute()) {
-        echo "Registration successful"; // Debug output
+        //echo "Registration successful"; // Debug output
         header("Location: login.php");
         exit;
-        echo "After redirection"; // Debug output (This won't be reached if redirection occurs)
+        //echo "After redirection"; // Debug output (This won't be reached if redirection occurs)
     } else {
         echo "Error: " . $stmt->error;
     }
