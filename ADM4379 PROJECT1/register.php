9bc2ac5bb0f8b80e979efc1ede7e39fb948d5c4f
gracehuuang <41273352+gracehuuang@users.noreply.github.com>
2024-03-20 16:03:39 -0400

    Update register.php

--- ADM4379 PROJECT1/register.php
+++ ADM4379 PROJECT1/register.php
@@ -1,8 +1,24 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
-require_once "connection.php";
+//require_once "connection.php";
+<?php
+// Database configuration
+$db_host = 'localhost'; // Change this to your database server hostname
+$db_user = 'root'; // Change this to your database username
+$db_password = 'root'; // Change this to your database password
+$db_db = 'MyTelferLife'; // Change this to your database name
+$db_port = '8889'; // Change this to your database port if needed
+
+// Create connection
+$conn = new mysqli($db_host, $db_user, $db_password, $db_db, $db_port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+?>
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $fname = $_POST['first_name'];
