18e0ba50ac57640e400c7ec04a867ceeecbe3df3
jacobaristotle10 <159086394+jacobaristotle10@users.noreply.github.com>
2024-03-28 19:05:48 -0400

    Update connection.php

--- ADM4379 PROJECT1/connection.php
+++ ADM4379 PROJECT1/connection.php
@@ -1,6 +1,6 @@
 <?php
     
-    $con= mysqli_connect('localhost', 'root', '', 'mtl_db');
+    $con= mysqli_connect('localhost', 'root', '', 'mytelferlife');
 
     if(!$con){
 
@@ -9,22 +9,3 @@
 
    
     ?>
-//other version don't know which one is better rn
-<?php
-$db_host = '127.0.0.1'; $db_user = 'root'; $db_db = 'MyTelferLife';
-// MAMP
-$db_password = 'root'; $db_port = '8889';
-$mysqli = new mysqli($db_host, $db_user, $db_password, $db_db, $db_port);
-if ($mysqli->connect_error) {
-echo 'Errno: '.$mysqli->connect_errno;
-echo '<br>';
-echo 'Error: '.$mysqli->connect_error;
-exit();
-}
-echo 'Success: A proper connection to MySQL was made.';
-echo '<br>';
-echo 'Host information: '.$mysqli->host_info;
-echo '<br>';
-echo 'Protocol version: '.$mysqli->protocol_version;
-$mysqli->close();
-?>
