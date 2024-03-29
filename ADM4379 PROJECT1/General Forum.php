6bfeed74a75567bc5248fbf054715963e49a9832
camikaci <156731310+camikaci@users.noreply.github.com>
2024-03-29 13:06:21 -0400

    Update General Forum.php

--- ADM4379 PROJECT1/General Forum.php
+++ ADM4379 PROJECT1/General Forum.php
@@ -35,7 +35,7 @@
             <nav>
                 <a href="#">Home</a>
                 <a href="General Forum.php" class="active">Forums</a>
-                <a href="index.php">Profile</a>
+                <a href="profile.php">Profile</a>
                 <a href="Course Search.php">Course Info</a>
                 <a href="login.php">Login</a>
             </nav>
