1d36bcff5740f26c718387de51c2faa62088a946
camikaci <156731310+camikaci@users.noreply.github.com>
2024-03-29 13:07:17 -0400

    Update register.php

--- ADM4379 PROJECT1/register.php
+++ ADM4379 PROJECT1/register.php
@@ -29,7 +29,7 @@
             <nav>
                 <a href="#">Home</a>
                 <a href="General Forum.php">Forums</a>
-                <a href="index.php">Profile</a>
+                <a href="profile.php">Profile</a>
                 <a href="Course Search.php">Course Info</a>
                 <a href="login.php">Login</a>
             </nav>
