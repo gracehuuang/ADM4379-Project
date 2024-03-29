1fbd13b435cd603a87e1001efb74c3c840bcf2fa
camikaci <156731310+camikaci@users.noreply.github.com>
2024-03-29 13:10:15 -0400

    Update Course Search.php

--- ADM4379 PROJECT1/Course Search.php
+++ ADM4379 PROJECT1/Course Search.php
@@ -16,7 +16,7 @@
     <header>
         <h1>Course Info</h1>
          <nav>
-                <a href="#">Home</a>
+                <a href="index.php">Home</a>
                 <a href="General Forum.php">Forums</a>
                 <a href="profile.php">Profile</a>
                 <a href="Course Search.php" class= 'active'>Course Info</a>
