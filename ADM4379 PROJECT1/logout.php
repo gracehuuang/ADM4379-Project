c920c19a5084cc466f127000c4592aaa361e8876
jacobaristotle10 <159086394+jacobaristotle10@users.noreply.github.com>
2024-04-02 20:41:14 -0400

    Update logout.php

--- ADM4379 PROJECT1/logout.php
+++ ADM4379 PROJECT1/logout.php
@@ -1,11 +1,6 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("Location: login.php");
-    exit;
-}
-
 $_SESSION = array();
 
 session_destroy();
